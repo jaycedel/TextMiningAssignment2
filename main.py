61a888bc4e239cc9694c9d8908c232db9437d473
Leonardo <junedelacruz@outlook.com>
2020-06-18 18:59:55 +1200

    latest parser with improvement

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import os
-import sys
 import xml.etree.ElementTree as ET
 import html.parser
+import nltk
 
 maleCount = 0
 femaleCount = 0
 
-path = "../blogs"
+path = "../blogs3" #FOLDER OF WHERE THE FILES ARE
 demographics = {}
 gender = {}
 age = {}
 interest = {}
 zodiac = {}
 
-for filename in os.listdir(path):
-    if not filename.endswith('.xml'):
-        continue
+#GET THE OBJECT OR THING ON THE BLOG POST AND RETURN THE OBJECT
+def parseBlog(txt):
+    topics = {}
+    #print(txt)
+    for sent in nltk.sent_tokenize(txt):
+        for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
+            if hasattr(chunk, 'label'):
+                topics[chunk.label()] = ' '.join(c[0] for c in chunk)
 
+    return topics
+
+def parseFileName(filename):
     splittedFilename = filename.split(".")
     if splittedFilename[1] in gender:
         gender[splittedFilename[1]] += 1
@@ -38,9 +46,16 @@ for filename in os.listdir(path):
     else:
         zodiac[splittedFilename[4]] = 1
 
+
+for filename in os.listdir(path):
+    if not filename.endswith('.xml'):
+        continue
+
+    parseFileName(filename)
+
     # GET THE FULLNAME
     fullname = os.path.join(path, filename)
-    print(fullname)
+    #print(fullname)
 
     # GET CONTENT OF THE XML FILE
     # https://stackoverflow.com/questions/42339876/error-unicodedecodeerror-utf-8-codec-cant-decode-byte-0xff-in-position-0-in/42340744
@@ -55,15 +70,18 @@ for filename in os.listdir(path):
         decodedContent = decodedContent.replace("<>", " ")
         decodedContent = decodedContent.replace("&lt;", " ")
         decodedContent = decodedContent.replace("&gt;", " ")
+        decodedContent = decodedContent.replace("urlLink", "")
         tree = ET.fromstring(decodedContent)
-        # for elem in tree.iter():
-        #     if elem.tag == 'post':
-        #         print(elem.text)
+        for elem in tree.iter():
+            if elem.tag == 'post':
+                topics = parseBlog(elem.text)
+                for keyTopic in topics:
+                    print(keyTopic, '->', topics[keyTopic])
 
     except Exception as e:
-            print("Unexpected error on " + fullname + ":", e)
-    #         raise
-            pass
+        print("Unexpected error on " + fullname + ":", e)
+        #         raise
+        pass
 
 print(gender)
 print(age)
