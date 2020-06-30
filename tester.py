95370cc7e7a5ca0b3517441fa710d55127a549d1
Leonardo <junedelacruz@outlook.com>
2020-07-01 10:05:36 +1200

    .

---
+++ tester.py
@@ -0,0 +1,7 @@
+from cleaner import clean_post
+
+from lda_topic_modelling import lda_parse_post
+
+message = ["carbon offset vatican forest fail reduc global warm uruguay tool need vulner climat chang ocean salti show global warm intensifi water cycl"]
+
+lda_parse_post(message)
